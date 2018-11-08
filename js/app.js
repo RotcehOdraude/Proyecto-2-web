a77224401bfb3ddbfffda4fe78a7766758882bd5
tavo <tavo@localhost.localdomain>
2018-11-08 00:49:22 -0600

    Primer commit :D

---
+++ js/app.js
@@ -0,0 +1,9 @@
+  $(document).ready(function(){
+    $('.sidenav').sidenav();
+    $(".dropdown-trigger").dropdown();
+    $('.carousel.carousel-slider').carousel({
+      fullWidth: true});
+
+  });
+
+
